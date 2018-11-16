50128818789c16261d48cc4d24c5c7bf08870382
Student <student@c010.local>
2018-11-16 17:01:31 +0300

    Initial Commit

---
+++ ahalo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ahalo
+//
+//  Created by Student on 16/11/2018.
+//  Copyright © 2018 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var TreeImageview: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
