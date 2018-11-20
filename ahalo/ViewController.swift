12d6a09c10938ac4b3c76b9ac5a23ffade5686bd
Student <student@c010.local>
2018-11-20 15:15:31 +0300

    apple

--- ahalo/ViewController.swift
+++ ahalo/ViewController.swift
@@ -9,13 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var listOfWords = [
+        "Арбуз",
+        "Банан",
+        "Гномик",
+        "Домик",
+        "Ель",
+        "Ёж",
+        "Железная Дорога"
+    ]
     
     @IBOutlet weak var TreeImageview: UIImageView!
+    @IBOutlet var letterButtons: [UIButton]!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    let incorrectMovesAllowed = 7
+    var totalWins = 0
+    var totalLosses = 0
+    
+    var game: Game!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        newRound()
     }
-
-
+    
+    func newRound ()
+    {
+        let word = listOfWords.removeFirst()
+        game = Game (word: word, incorrectMovesRemaining:
+            incorrectMovesAllowed  )
+    }
+    func updateUI ()  {
+        let name = "Tree 5"
+        TreeImageview.image = UIImage(named: name)
+        scoreLabel.text = "Выигрыши: \(totalWins), Проигрыши: \(totalLosses)"
+        
+    }
+    @IBAction func ButtonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
+    
+    
 }
+
 
---
+++ ahalo/game.swift
@@ -0,0 +1,13 @@
+//
+//  game.swift
+//  ahalo
+//
+//  Created by Student on 20/11/2018.
+//  Copyright © 2018 Student. All rights reserved.
+//
+
+import Foundation
+struct Game {
+    var word:String
+    var incorrectMovesRemaining: Int
+}
